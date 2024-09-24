c9423e40c2f5376d34b97ddf2c40bd29d066a101
vvssgowtham <gowthamvvss@gmail.com>
2024-09-24 19:55:19 +0530

    Add functions and HTML files for fetching and displaying posts and comments
    - Added fetchPosts function to fetch and display posts from API.
    - Created fetchPosts.html to display fetched posts.
    - Created user-posts-comments.html to fetch and display user-specific posts and comments.
    - Added user-posts-comments.js to fetch and display user-specific posts and comments.
    - Added APIRequestsPostman.js to demonstrate API interactions using Postman.

---
+++ API/APIRequestsPostman.js
@@ -0,0 +1,6 @@
+// Objective: Understand how to interact with APIs by making GET and POST requests in Postman,
+// analyzing response status codes, headers, and bodies, and exploring additional HTTP methods to manipulate resources programmatically.
+
+// Task 62 :
+
+// https://jsonplaceholder.typicode.com/posts/ This end point got hit with different HTTP methods and works as required i.e GET for getting the content and POST for Insertion of content.
